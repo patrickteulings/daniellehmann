a33c15d6c59363c26a7f3a4fe58b1a7f61f564f2
Patrick Teulings <patrickteulings@gmail.com>
2021-07-14 13:50:27 +0200

    Added 'Made By' in footer

--- footer.php
+++ footer.php
@@ -104,7 +104,10 @@
     <?= file_get_contents(__DIR__ . '/assets/images/circle-large.svg'); ?>
   </div>
 </footer><!-- #colophon -->
-
+<div class="madeby">made with
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart">
+    <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
+  </svg> by <a href="https://www.patrickteulings.nl">patrickteulings</a>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
