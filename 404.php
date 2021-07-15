de71de774d2676c02b31fc25b4a86a9a07073fa9
Patrick Teulings <patrickteulings@gmail.com>
2021-07-15 20:22:02 +0200

    Added static hero-image to 404 page

--- 404.php
+++ 404.php
@@ -12,22 +12,25 @@
 
 get_header();
 ?>
+<article class="entry">
+  <header class="page-header alignwide hero">
+    <div class="hero__inner">
+      <div class="hero-content">
 
-<header class="page-header alignwide hero">
-  <div class="hero__inner">
-    <div class="hero-content">
-
-      <div class="hero__title--mini">404</div>
-      <div class="hero__title">
-        <p>Hi! De pagina die je zocht,
-        <h1>bestaat helaas niet.</h1>
-        <p>probeer via het menu de juiste pagina te vinden of gebruik de zoekoptie!</p>
+        <div class="hero__title--mini">404</div>
+        <div class="hero__title">
+          <p>Hi! De pagina die je zocht,
+          <h1>bestaat helaas niet.</h1>
+          <p>probeer via het menu de juiste pagina te vinden of gebruik de zoekoptie!</p>
+        </div>
+        <?php get_template_part('template-parts/header/hero-404'); ?>
       </div>
-      <?php get_template_part('template-parts/header/hero-404'); ?>
+      <div class="hero-image">
+        <div class="hero-image__inner js-hero-image" data-image="<?= get_stylesheet_directory_uri() . '/assets/images/static/page-404-hero.jpg'; ?>"></div>
+      </div>
     </div>
-  </div>
-</header><!-- .page-header -->
-
+  </header><!-- .page-header -->
+</article>
 
 <?php
 get_footer();
